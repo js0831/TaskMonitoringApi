0999625cd14bcbf127e2658d02e18b1ff5172671
81254807 <81254807@corp.root>
2019-08-02 09:20:29 +0800

    update cors middleware allow all access control methods on response

--- lib/middlewares/CorsSetup.ts
+++ lib/middlewares/CorsSetup.ts
@@ -4,6 +4,7 @@ export const corsSetup = (req, res, next) => {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
+    res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
     
     if(req.method === "OPTIONS"){
         req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
